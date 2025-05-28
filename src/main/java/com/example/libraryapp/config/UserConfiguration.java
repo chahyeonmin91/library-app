7da25e245ec2f913032ae4eec98466f4ddbaf66c
Chahyeon Min <chahyeonmin91@gmail.com>
2025-05-28 13:43:03 +0900

    jpa

--- src/main/java/com/example/libraryapp/config/UserConfiguration.java
+++ src/main/java/com/example/libraryapp/config/UserConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.libraryapp.config;
 
-import com.example.libraryapp.repository.user.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
